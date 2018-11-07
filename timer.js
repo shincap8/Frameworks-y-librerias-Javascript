c1df0e2759d1c6e1e6f027d8cf63858a59bdf36d
shincap8 <43832784+shincap8@users.noreply.github.com>
2018-11-07 16:09:29 -0500

    timer listo

---
+++ timer.js
@@ -0,0 +1,29 @@
+var interval;
+var terminado;
+function countdown() {
+  clearInterval(interval);
+  interval = setInterval( function() {
+      var timer = $('#timer').html();
+      timer = timer.split(':');
+      var minutes = timer[0];
+      var seconds = timer[1];
+      seconds -= 1;
+      if (minutes < 0) return;
+      else if (seconds < 0 && minutes != 0) {
+          minutes -= 1;
+          seconds = 59;
+      }
+      else if (seconds < 10 && length.seconds != 2) seconds = '0' + seconds;
+
+      $('#timer').html(minutes + ':' + seconds);
+
+      if (minutes == 0 && seconds == 0) clearInterval(interval), terminado = true;
+  }, 1000);
+}
+function reset() {
+    $('#timer').text("2:00");
+    clearInterval(interval);
+    terminado = false;
+}
+//Cuando se quiere iniciar el conteo se llama la función countdown();
+//Cuando se quiere detener y reiniciar se llama la función clearInterval(interval)
