1bb71f1caa413e500497dab1be14968bf28388db
shincap8 <43832784+shincap8@users.noreply.github.com>
2018-11-07 16:29:58 -0500

    cogiendo forma
    Ya esta lista la función para hacer titilar los dulces iguales y eliminarlos, ya esta lista la función del puntaje

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 $(function () {
     tituloamarillo()
     tableroInicio()
-    eliminarH()
-    eliminarV()
 })
 
 //Función de Animacion del Titulo Principal
@@ -22,91 +20,105 @@ function tituloamarillo() {
 };
 //------------------------------------------
 //Función para el tablero inicial 
+var id = 1;
 function tableroInicio() {
     var cantidad = 7
-    var id = 1
     for (var j = 1; j <= cantidad; j++) {
         for (var i = 0; i < cantidad; i++) {
             var aleatorio = Math.round(Math.random() * (4 - 1) + parseInt(1))
             var pick = "image/" + aleatorio.toString() + ".png"
             if ($('.col-' + j + ' img').length < 7) {
-                $('.col-' + j).append('<img src="' + pick + '" class="elemento" id="'+id+'">');
+                $('.col-' + j).append('<img src="' + pick + '" class="elemento" id="' + id + '">');
             } id++;
-        } 
+        }
     }
-    return id; 
 }
 //-------------------------------------------
+
 //Funciones para comparar columnas y filas
 function derecha() {
     var cantidad = 7;
-    var grid = [];
+    var numero = 0;
     for (var i = 0; i < cantidad; i++) {
-        grid[i] = [];
         for (var j = 1; j < cantidad; j++) {
             var objeto = $('.col-' + j + ' .elemento');
             if ((j + 2) <= 7 && (objeto[i].src == $('.col-' + (j + 1) + ' .elemento')[i].src) && ((objeto[i].src == $('.col-' + (j + 2) + ' .elemento')[i].src))) {
                 for (var k = 0; k < cantidad; k++) {
                     if ((j + k) <= 7 && objeto[i].src == $('.col-' + (j + k) + ' .elemento')[i].src) {
-                        grid[i][j + k] = $('.col-' + (j + k) + ' .elemento')[i].id;
+                        numero = '#' + $('.col-' + (j + k) + ' .elemento')[i].id
+                        $(numero).addClass('eliminar')
                     } else break
                 }
             }
         }
     }
-    return grid;
 }
 
 function abajo() {
     var cantidad = 7;
-    var grid = [];
+    var numero = 0;
     for (var j = 1; j <= cantidad; j++) {
-        grid[j] = [];
         for (var i = 0; i < cantidad; i++) {
             var objeto = $('.col-' + j + ' .elemento');
             if ((i + 2) < 7 && (objeto[i].src == objeto[i + 1].src) && (objeto[i].src == objeto[i + 2].src)) {
                 for (var k = 0; k < cantidad; k++) {
                     if ((i + k) < 7 && objeto[i].src == objeto[i + k].src) {
-                        grid[j][i + k] = objeto[i + k].id;
+                        numero = '#' + objeto[i + k].id;
+                        $(numero).addClass('eliminar')
                     } else break
                 }
             }
         }
     }
-    return grid;
 }
 //-------------------------------------------------------------------------------------
 //Funciones para eliminar Dulces iguales
-function eliminarH() {
-    var cantidad = 7;
-    var gridX = derecha();
-    var id = 0;
-    console.log(gridX);
-    for (i = 0; i < cantidad; i++) {
-        if (gridX[i].length != 0) {
-            for (var j = 1; j <= gridX.length; j++) {
-                if (gridX[i][j] != 0) {
-                    id = '#'+gridX[i][j]
-                    $(id).addClass('eliminar')
+function titilar() {
+    if ($('.eliminar').length!=0) {
+        $('.eliminar').animate({ opacity: 0 }, 300)
+            .animate({ opacity: 1 }, 300)
+            .animate({ opacity: 0 }, 300)
+            .animate({ opacity: 1 }, 300)
+            .animate({ opacity: 0 }, 300)
+            .animate({ opacity: 1 }, 300)
+            .animate({ opacity: 0 }, 300)
+            .animate({ opacity: 1 }, 300)
+            .animate({ opacity: 0 }, 300)
+            .animate({ opacity: 1 }, 300)
+            .animate({ opacity: 0 }, 350,
+                function () {
+                    puntaje()
+                    $('.eliminar').remove()
+                    setTimeout(rellenar, 250)
                 }
+            )
+        derecha()
+        abajo() 
+    } else return false;
+}
+//-----------------------------------------------------------------------------------
+//Funcion para añadir despues de eliminar iguales
+function rellenar() {
+    var cantidad = 7
+    var fill = 0
+    for (i = 1; i <= cantidad; i++) {
+        if ($('.col-' + i + ' img').length < 7) {
+            fill = cantidad - $('.col-' + i + ' img').length
+            for (j = 0; j < fill; j++) {
+                var aleatorio = Math.round(Math.random() * (4 - 1) + parseInt(1))
+                var pick = "image/" + aleatorio.toString() + ".png"
+                $('.col-' + i).prepend('<img src="' + pick + '" class="elemento" id="' + id + '">');
+                id++
             }
         }
     }
+}
+//-------------------------------------------------------------------------------------
+//Funcion para calcular puntaje
+var puntos = 0;
+function puntaje() { 
+    puntos = puntos + ($('.eliminar').length * 15)
+    $('#score-text').text(puntos)
 }
 
-function eliminarV() {
-    var cantidad = 7;
-    var gridY = abajo();
-    var id = 0;
-    console.log(gridY);
-    for (j = 1; j <= cantidad; j++) {
-        if (gridY[j].length != 0) {
-            for (var i = 1; i < gridY.length; i++) {
-                if (gridY[j][i] != 0) {
-                    id = '#' + gridY[j][i]
-                    $(id).addClass('eliminar')
-                }
-            }
-        }
-    }
-}
+
