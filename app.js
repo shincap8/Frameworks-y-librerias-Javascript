88bced8f9772b2391dae1ea10a63509d1532b7dc
shincap8 <43832784+shincap8@users.noreply.github.com>
2018-11-06 19:30:28 -0500

    memoria de ubicacion despues de comparacion

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 $(function () {
     tituloamarillo()
     tableroInicio()
-    derecha()
-    abajo()
+    eliminarH()
+    eliminarV()
 })
 
 //Función de Animacion del Titulo Principal
@@ -24,52 +24,89 @@ function tituloamarillo() {
 //Función para el tablero inicial 
 function tableroInicio() {
     var cantidad = 7
-    for (var j = 0; j <= cantidad; j++) {
+    var id = 1
+    for (var j = 1; j <= cantidad; j++) {
         for (var i = 0; i < cantidad; i++) {
             var aleatorio = Math.round(Math.random() * (4 - 1) + parseInt(1))
             var pick = "image/" + aleatorio.toString() + ".png"
             if ($('.col-' + j + ' img').length < 7) {
-                $('.col-' + j).append('<img src="' + pick + '", class="elemento">');
-            }
-        }
+                $('.col-' + j).append('<img src="' + pick + '" class="elemento" id="'+id+'">');
+            } id++;
+        } 
     }
+    return id; 
 }
 //-------------------------------------------
-//Función para verificar columnas y filas
-
+//Funciones para comparar columnas y filas
 function derecha() {
     var cantidad = 7;
     var grid = [];
     for (var i = 0; i < cantidad; i++) {
-        grid[i]=[];
+        grid[i] = [];
         for (var j = 1; j < cantidad; j++) {
             var objeto = $('.col-' + j + ' .elemento');
             if ((j + 2) <= 7 && (objeto[i].src == $('.col-' + (j + 1) + ' .elemento')[i].src) && ((objeto[i].src == $('.col-' + (j + 2) + ' .elemento')[i].src))) {
                 for (var k = 0; k < cantidad; k++) {
                     if ((j + k) <= 7 && objeto[i].src == $('.col-' + (j + k) + ' .elemento')[i].src) {
-                        grid[i][j+k]= 1;
+                        grid[i][j + k] = $('.col-' + (j + k) + ' .elemento')[i].id;
                     } else break
                 }
-            } 
-        }      
-    } console.log(grid);
+            }
+        }
+    }
+    return grid;
 }
 
 function abajo() {
     var cantidad = 7;
     var grid = [];
-    for (var j = 1; j < cantidad; j++) {
+    for (var j = 1; j <= cantidad; j++) {
         grid[j] = [];
         for (var i = 0; i < cantidad; i++) {
             var objeto = $('.col-' + j + ' .elemento');
             if ((i + 2) < 7 && (objeto[i].src == objeto[i + 1].src) && (objeto[i].src == objeto[i + 2].src)) {
                 for (var k = 0; k < cantidad; k++) {
-                    if ((i + k) < 7 && objeto[i].src == objeto[i+k].src) {
-                        grid[j][i + k] = 1;
-                    }else break
+                    if ((i + k) < 7 && objeto[i].src == objeto[i + k].src) {
+                        grid[j][i + k] = objeto[i + k].id;
+                    } else break
                 }
             }
         }
-    }console.log(grid);
+    }
+    return grid;
 }
-//--------------------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------
+//Funciones para eliminar Dulces iguales
+function eliminarH() {
+    var cantidad = 7;
+    var gridX = derecha();
+    var id = 0;
+    console.log(gridX);
+    for (i = 0; i < cantidad; i++) {
+        if (gridX[i].length != 0) {
+            for (var j = 1; j <= gridX.length; j++) {
+                if (gridX[i][j] != 0) {
+                    id = '#'+gridX[i][j]
+                    $(id).addClass('eliminar')
+                }
+            }
+        }
+    }
+}
+
+function eliminarV() {
+    var cantidad = 7;
+    var gridY = abajo();
+    var id = 0;
+    console.log(gridY);
+    for (j = 1; j <= cantidad; j++) {
+        if (gridY[j].length != 0) {
+            for (var i = 1; i < gridY.length; i++) {
+                if (gridY[j][i] != 0) {
+                    id = '#' + gridY[j][i]
+                    $(id).addClass('eliminar')
+                }
+            }
+        }
+    }
+}
